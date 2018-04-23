b30e1eb7b5e89262cd5c8425e0767bfebea815a1
Alexander <avi2011class@yandex.ru>
2018-04-23 17:08:08 +0300

    some small fixes, new encryption features

--- ecrypt_token.py
+++ ecrypt_token.py
@@ -2,18 +2,34 @@
 
 import aes
 import getpass
+import sys
 
-token = input('token: ')
-key = getpass.getpass(prompt='Password:')
-filename = input('Filename: ')
 
-cipher = aes.AESCipher(key=key)
-ss = cipher.encrypt(token)
+class Encryptor:
 
-f = open(filename, 'w')
-f.write(ss)
+    @staticmethod
+    def encrypt_and_save(token, key, filename):
+        cipher = aes.AESCipher(key=key)
+        encrypted_token = cipher.encrypt(token)
+        f = open(filename, 'w')
+        f.write(encrypted_token)
+        f.close()
 
-f.close()
+    @staticmethod
+    def decrypt_and_return(key, filename):
+        cipher = aes.AESCipher(key=key)
+        encrypted_token = open(filename, 'r').read()
+        token = cipher.decrypt(encrypted_token)
+        return token
 
-print('Ecrypted token was written to', filename)
 
+if __name__ == "__main__":
+    if not '-d' in sys.argv:
+        print("Encryption mode")
+        Encryptor.encrypt_and_save(input('Token: '),
+                                   getpass.getpass(prompt='Password: '),
+                                   input('Filename: '))
+    else:
+        print("Decryption mode")
+        print(Encryptor.decrypt_and_return(getpass.getpass(prompt='Password: '),
+                                           input('Filename: ')))
